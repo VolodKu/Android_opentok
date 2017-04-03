45dd5cbeef0f32967c21f0ac2c7d98a2db808b76
VolodKu <VoldKu>
2017-04-04 02:31:21 +0800

    Android Opentok Screenshaing Sample

---
+++ TestDemo/app/src/main/java/com/volodku/testdemo/OpenTokConfig.java
@@ -0,0 +1,8 @@
+package com.volodku.testdemo;
+
+public class OpenTokConfig {
+
+    public static final String API_KEY = "Test API Key";
+    public static final String SESSION_ID = "Test Session Key";
+    public static final String TOKEN = "Test Token";
+}
